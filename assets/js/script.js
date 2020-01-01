9f6807738bf3d459b1ce2a46af3564d4743c0f03
Ian Rones <ronesp.85@gmail.com>
2020-01-01 14:24:45 -0800

    [add] added click to span function, now removes the parent li when clicking on X, includes a fadeOut timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 $('li').click(function() {
    $(this).toggleClass("completed")
 })
+
+// Click on X to delete Task
+$("span").click(function(){
+    $(this).parent().fadeOut(1000,function(){
+      $(this).remove()  
+    });
+    event.stopPropagation()
+})
